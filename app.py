a8a4470fe0592682d1fdcdf0a02fe80a802440ab
mmmwembe <mmm111@hotmail.com>
2024-12-01 19:33:35 -0500

    changes

--- app.py
+++ app.py
@@ -83,6 +83,9 @@ def ensure_data_initialized():
             app.logger.warning("Data structures not initialized, reinitializing...")
             initialize_data()
 
+# Initial data load on module import
+initialize_data()
+
 
 # Create an instance of GCPOps
 gcp_ops = GCPOps()
@@ -463,10 +466,10 @@ def download_labels():
 def display_json():
     return render_template('displayjson.html')
 
-# Initialize data when the app starts
-@app.before_first_request
-def setup():
-    initialize_data()
+# # Initialize data when the app starts
+# @app.before_first_request
+# def setup():
+#     initialize_data()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
